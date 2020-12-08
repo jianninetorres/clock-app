e3a3d3d08f25811190c6148f023e247bbf393777
Jiannine Torres <jiannine@agilebits.com>
2020-12-08 08:32:18 -0500

    Pass transformY prop to MainSection to determine visibility and height

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -3,17 +3,26 @@ import React from "react";
 import styled from "styled-components";
 
 const MainSectionStyles = styled.section`
-  @if ${(props) => props.buttonState === true} {
-    display: block;
-  } @else {
-    display: none;
-  }
-  padding: 32px;
+  display: block;
+  padding: ${(props) => props.buttonState};
+  flex: 1 1 auto;
+  width: 100%;
+  height: 100%;
+  max-height: ${(props) => props.maxHeight};
+  border: 1px solid red;
+  position: absolute;
+  bottom: 0;
+  transition: max-height 0.5s ease-out;
 `;
 
 const MainSection = ({ transformY }) => {
+  const isVisible = transformY === true ? "32px" : "0";
+  const maxHeight = transformY === true ? "200px" : 0;
+
   return (
-    <MainSectionStyles buttonState={transformY}>main styles</MainSectionStyles>
+    <MainSectionStyles buttonState={isVisible} maxHeight={maxHeight}>
+      main styles
+    </MainSectionStyles>
   );
 };
 
