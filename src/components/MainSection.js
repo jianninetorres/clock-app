da84bd7938457183bf249555773a35f701377dd2
Jiannine Torres <jiannine@agilebits.com>
2020-12-08 18:26:36 -0500

    Wrap MainSection grid in container

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -2,8 +2,7 @@ import React from "react";
 
 import styled from "styled-components";
 
-const MainSectionStyles = styled.section`
-  display: grid;
+const DivContainerStyles = styled.div`
   padding: ${(props) => props.buttonState};
   flex: 1 1 auto;
   width: 100%;
@@ -13,9 +12,27 @@ const MainSectionStyles = styled.section`
   position: absolute;
   bottom: 0;
   transition: max-height 0.5s ease-out;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const MainSectionStyles = styled.section`
+  display: grid;
+  width: 100%;
+  height: 100%;
 
   @media screen and (min-width: 568px) {
     grid-template-columns: 1fr 1fr;
+    padding: {
+      left: calc(var(--base-size) * 2);
+      right: calc(var(--base-size) * 2);
+    }
+  }
+
+  @media screen and (min-width: 1200px) {
+    max-width: 1100px;
+    margin: 0 auto;
   }
 
   div {
@@ -31,12 +48,22 @@ const MainSectionStyles = styled.section`
       align-items: flex-start;
     }
 
+    @media screen and (min-width: 768px) {
+      &:nth-child(odd) {
+        border-right: 1px solid grey;
+      }
+    }
+
     h3 {
       font-weight: 600;
       letter-spacing: 1px;
       margin-bottom: 0;
       text-transform: capitalize;
       color: ${(props) => props.h3FontColour};
+      @media screen and (min-width: 568px) {
+        font-size: 2.5rem;
+        margin-top: 16px;
+      }
     }
 
     h4 {
@@ -62,38 +89,41 @@ const MainSection = ({
   const h4FontColour = icon === "sun" ? "var(--grey1)" : "var(--grey2)";
 
   return (
-    <MainSectionStyles
+    <DivContainerStyles
       buttonState={isVisible}
       maxHeight={maxHeight}
       bgColour={bgColour}
-      h3FontColour={h3FontColour}
-      h4FontColour={h4FontColour}
     >
-      {timezone ? (
-        <div>
-          <h4>Current timezone</h4>
-          <h3>{timezone}</h3>
-        </div>
-      ) : null}
-      {dayOfYear ? (
-        <div>
-          <h4>Day of the year</h4>
-          <h3>{dayOfYear}</h3>
-        </div>
-      ) : null}
-      {dayOfWeek ? (
-        <div>
-          <h4>Day of the week</h4>
-          <h3>{dayOfWeek}</h3>
-        </div>
-      ) : null}
-      {weekNumber ? (
-        <div>
-          <h4>Week number</h4>
-          <h3>{weekNumber}</h3>
-        </div>
-      ) : null}
-    </MainSectionStyles>
+      <MainSectionStyles
+        h3FontColour={h3FontColour}
+        h4FontColour={h4FontColour}
+      >
+        {timezone ? (
+          <div>
+            <h4>Current timezone</h4>
+            <h3>{timezone}</h3>
+          </div>
+        ) : null}
+        {dayOfYear ? (
+          <div>
+            <h4>Day of the year</h4>
+            <h3>{dayOfYear}</h3>
+          </div>
+        ) : null}
+        {dayOfWeek ? (
+          <div>
+            <h4>Day of the week</h4>
+            <h3>{dayOfWeek}</h3>
+          </div>
+        ) : null}
+        {weekNumber ? (
+          <div>
+            <h4>Week number</h4>
+            <h3>{weekNumber}</h3>
+          </div>
+        ) : null}
+      </MainSectionStyles>
+    </DivContainerStyles>
   );
 };
 
